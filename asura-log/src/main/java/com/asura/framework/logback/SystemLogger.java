52e3e05493c8cd69122ee97520ad8bde72a44c4a
zhangshaobin <zhang1bin.2090@qq.com>
2016-01-28 20:24:22 +0800

    接入cat

--- asura-log/src/main/java/com/asura/framework/logback/SystemLogger.java
+++ asura-log/src/main/java/com/asura/framework/logback/SystemLogger.java
@@ -18,6 +18,8 @@ import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.dubbo.rpc.RpcContext;
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Transaction;
 
 /**
  *
@@ -51,7 +53,9 @@ public class SystemLogger {
 
 	@Around("execution(* com.ziroom..*.proxy.*.* (..))")
 	public Object doBasicProfiling(final ProceedingJoinPoint joinPoint) throws Throwable {
-		
+		long start_all = System.currentTimeMillis();
+		long end_all = 0L;
+		Transaction tran = Cat.newTransaction("Aspect-proxy", "proxy-method");
 		if (RpcContext.getContext().getRemoteAddressString() != null && RpcContext.getContext().getMethodName() != null
 				&& RpcContext.getContext().getUrl() != null) {
 			MDC.put(HOST, RpcContext.getContext().getRemoteAddressString());
@@ -77,17 +81,33 @@ public class SystemLogger {
 			long end = System.currentTimeMillis();
 			// 记录耗时
 			logger.info(de.toJsonStr()+" 耗时:" + (end - start) + " ms");
+			Cat.logEvent(joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), "0", de.toJsonStr()+" 耗时:" + (end - start) + " ms");
+			/**
+			 * 设置消息的状态,必须设置，0:标识成功,其他标识发生了异常
+			 */
+			tran.setStatus(Transaction.SUCCESS);
+			end_all = System.currentTimeMillis();
 			return retVal;
 		} catch (final Exception e) {
 			final ErrorLogEntity ele = new ErrorLogEntity(de);
 			ele.setThrowMessage(e.toString());
-			//抛出异常日志
 			logger.error(ele.toJsonStr());
+			/**
+			 * cat使用log4j记录异常信息
+			 */
+			Cat.logError(ele.toJsonStr(), e);
+			/**
+			 * 发生异常了，要设置消息的状态为e
+			 */
+			tran.setStatus(e);
+			end_all = System.currentTimeMillis();
 			throw e;
 		} finally {
 			MDC.remove(HOST);
 			MDC.remove(INTERFACE);
 			MDC.remove(METHOD);
+			tran.complete();
+			logger.info("接入cat后整体耗时:" + (end_all - start_all) + " ms");
 		}
 	}
 }
