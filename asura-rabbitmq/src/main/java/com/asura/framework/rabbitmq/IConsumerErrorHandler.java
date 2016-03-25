759f280345fb1101ececf8fb3624b3b0ef14deac
liusq23 <liusq23@homelink.com.cn>
2016-03-25 13:16:17 +0800

    Exchange name

---
+++ asura-rabbitmq/src/main/java/com/asura/framework/rabbitmq/IConsumerErrorHandler.java
@@ -0,0 +1,35 @@
+/**
+ * @FileName: IConsumerErrorHandler.java
+ * @Package: com.asura.framework.rabbitmq
+ * @author sence
+ * @created 3/23/2016 4:09 PM
+ * <p/>
+ * Copyright 2015 ziroom
+ */
+package com.asura.framework.rabbitmq;
+
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * <p>消费错误处理类</p>
+ *
+ * <PRE>
+ * <BR>	修改记录
+ * <BR>-----------------------------------------------
+ * <BR>	修改日期			修改人			修改内容
+ * </PRE>
+ *
+ * @author sence
+ * @since 1.0
+ * @version 1.0
+ */
+public interface IConsumerErrorHandler {
+
+    /**
+     * 消息错误处理
+     * @param delivery
+     */
+    void handlerErrorMessage(QueueingConsumer.Delivery delivery);
+
+
+}
